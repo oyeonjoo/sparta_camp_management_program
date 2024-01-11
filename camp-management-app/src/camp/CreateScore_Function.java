7e522f407546ad7c5672dd90b9dcb45b61d44e05
HEEEUN9812 <heeeun9812@gmail.com>
2024-01-11 16:54:53 +0900

    addStudentScore 메서드 추가

--- camp-management-app/src/camp/CreateScore_Function.java
+++ camp-management-app/src/camp/CreateScore_Function.java
@@ -5,6 +5,7 @@ import camp.model.Student;
 import camp.model.Subject;
 
 import java.util.List;
+import java.util.Scanner;
 
 public class CreateScore_Function {
         private static List<Student> studentStore;
@@ -31,42 +32,58 @@ public class CreateScore_Function {
             return this.ScoreStore;
         }
 
-        // 점수 -> 등급 클래스
-        public String scoreToGrade(List<Score> scores){
-            String grade = null;
-            switch (subjectType){
-                case "MANDATORY":
-                    if(scores >= 95){
-                        grade = "A";
-                    }else if ((scores >= 90)){
-                        grade = "B";
-                    }else if (scores >= 80){
-                        grade = "C";
-                    }else if (scores >= 70){
-                        grade = "D";
-                    } else if (scores >= 60) {
-                        grade = "F";
-                    }else {
-                        grade = "N";
-                    }
-
-                case "CHOICE":
-                    if(scores >= 90){
-                        grade = "A";
-                    }else if ((scores >= 80)){
-                        grade = "B";
-                    }else if (scores >= 70){
-                        grade = "C";
-                    }else if (scores >= 60){
-                        grade = "D";
-                    } else if (scores >= 50) {
-                        grade = "F";
-                    }else {
-                        grade = "N";
-                    }
-            }
-            return grade;
+        public void addStudentScore (String studentId){
+            Scanner sc = new Scanner(System.in);
+            Student student = new Student();
+            student.getsubjectlist();
+            // 과목 입력
+            System.out.println("과목 번호를 입력해주세요.");
+            int inputSubject = sc.nextInt();
+            // 회차 입력
+            System.out.println("시험 회차를 입력해주세요.");
+            int inputExamGround = sc.nextInt();
+            // 점수 입력
+            System.out.println("시험 점수를 입력해주세요");
+            int inputScore = sc.nextInt();
+            student.addScore(inputSubject, student.getScorelist(inputSubject).add(inputExamGround, inputScore));
         }
+
+        // 점수 -> 등급 클래스
+//        public String scoreToGrade(List<Score> scores){
+//            String grade = null;
+//            switch (subjectType){
+//                case "MANDATORY":
+//                    if(scores >= 95){
+//                        grade = "A";
+//                    }else if ((scores >= 90)){
+//                        grade = "B";
+//                    }else if (scores >= 80){
+//                        grade = "C";
+//                    }else if (scores >= 70){
+//                        grade = "D";
+//                    } else if (scores >= 60) {
+//                        grade = "F";
+//                    }else {
+//                        grade = "N";
+//                    }
+//
+//                case "CHOICE":
+//                    if(scores >= 90){
+//                        grade = "A";
+//                    }else if ((scores >= 80)){
+//                        grade = "B";
+//                    }else if (scores >= 70){
+//                        grade = "C";
+//                    }else if (scores >= 60){
+//                        grade = "D";
+//                    } else if (scores >= 50) {
+//                        grade = "F";
+//                    }else {
+//                        grade = "N";
+//                    }
+//            }
+//            return grade;
+//        }
 
     }
 
