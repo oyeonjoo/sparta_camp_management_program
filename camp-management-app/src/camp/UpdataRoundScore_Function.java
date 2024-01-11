c0b4879d9350ff538d9e3052839d4dddecc9689e
stidy <gstudy1021@gmail.com>
2024-01-11 21:10:10 +0900

    test

--- camp-management-app/src/camp/UpdataRoundScore_Function.java
+++ camp-management-app/src/camp/UpdataRoundScore_Function.java
@@ -11,8 +11,13 @@ import java.util.List;
         private static List<Subject> subjectStore;
         private static List<Score> ScoreStore;
         private String studentId;
-        public void functions() {
+        public void functions() {    // 과목별 회차 점수 수정  ( 과목, 회차, 이름, 점수)
             // 여기에 작업하시요
+            // 수학 3회차 점수는 40 이고 4회차는 70이면 70으로 변경
+            // 국어 2회차 점수가 70이고 3회차 점수는 60이면 60으로 변경
+            // 회차를 비교해서 바뀐 점수를 반영한다.
+            // 과목별 회차 점수 수정  ( 과목, 회차, 이름, 점수)
+
 
         }
         UpdataRoundScore_Function(String studentId,List<Student> studentStore,List<Subject> subjectStore,List<Score> ScoreStore){
