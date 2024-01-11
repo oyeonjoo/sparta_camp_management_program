3ae4d0be31695154b8a4b2f3ea91c0176e312992
oyeonjoo <oyj0842@naver.com>
2024-01-12 01:31:46 +0900

    수강생 목록 조회

--- camp-management-app/src/camp/StudentView.java
+++ camp-management-app/src/camp/StudentView.java
@@ -17,15 +17,20 @@ public class StudentView {
         this.ScoreStore = ScoreStore;
     }
     public void functions() {
-        // 여기에 작업하시요//
-    }
-    public List<Student> GetstudentStore(){
-        return this.studentStore;
-    }
-    public List<Subject> GetsubjectStore(){
-        return this.subjectStore;
-    }
-    public List<Score> GetScoreStore(){
-        return this.ScoreStore;
+        // 여기에 작업하시요
+        // 고유 번호, 이름을 조회한다.
+        Scanner sc = new Scanner(System.in);
+        System.out.println("학생 번호를 입력하세요.");
+        String studentId = sc.nextLine();
+        // 학생을 찾는다. 해당 학생의 학생번호와 이름을 출력한다.
+        int num = 1;
+        for(Student s : studentStore) {
+            if(s.getStudentId().equals(studentId)) {
+                num = 2;
+                System.out.println("학생 번호 : " + s.getStudentId() + "학생 이름: " + s.getStudentName());
+            }
+        } if(num == 1) {
+            System.out.println("학생을 찾지 못했습니다.");
+        }
     }
 }
