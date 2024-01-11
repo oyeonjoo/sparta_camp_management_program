f96264cb73296a7e15a65dbf6cc13cd1e9a7c630
HEEEUN9812 <heeeun9812@gmail.com>
2024-01-11 11:54:59 +0900

    01/11

--- camp-management-app/src/main/java/camp/StudentRegister.java
+++ camp-management-app/src/main/java/camp/StudentRegister.java
@@ -191,7 +191,7 @@ public class StudentRegister {
             while (flag) {
                 System.out.println("==================================");
                 System.out.println("점수 관리 실행 중...");
-                System.out.println("1. 수강생의 과목별 시험 회차 및 점수 등록");
+                System.out.println("1. 수강생의 과목별 시험 회차 및 점수 등록"); // 희은
                 System.out.println("2. 수강생의 과목별 회차 점수 수정");
                 System.out.println("3. 수강생의 특정 과목 회차별 등급 조회");
                 System.out.println("4. 메인 화면 이동");
@@ -218,9 +218,10 @@ public class StudentRegister {
 
         // 수강생의 과목별 시험 회차 및 점수 등록
         private static void createScore() {
-            String studentId = getStudentId(); // 관리할 수강생 고유 번호
-            System.out.println("시험 점수를 등록합니다...");
+            String studentId = getStudentId();
+            System.out.println("시험 점수를 등록합니다.");
             // 기능 구현
+
             System.out.println("\n점수 등록 성공!");
         }
 
